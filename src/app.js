7c813b42b5b82b901fd0754306f2b1f7c5ed305f
jzj083271930 <lt258110166@126.com>
2024-09-17 22:26:41 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -192,7 +192,7 @@ const push = (title, desp) => {
 
 // 开始执行程序
 async function main() {
- var accounts2=[{"userName":"15302721710","password":"liutao092716"},{"userName":"15361575573","password":"lT#20180516"}];
+ var accounts2=[{"userName":"15302721710","password":"liutao092716"},{"userName":"15361575573","password":"lT#20180516"},{"userName":"15302772036","password":"lT#20180516"}];
   for (let index = 0; index < accounts2.length; index += 1) {
     const account = accounts2[index];
     const { userName, password } = account;
